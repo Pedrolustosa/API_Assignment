3786b65fafe213fb61f9d3b00b0223aada2a1259
Pedrolustosa <pedroeternalss@gmail.com>
2022-11-07 20:05:08 -0300

    feat: Completing the Minimal API creation

--- API_Assignment/Program.cs
+++ API_Assignment/Program.cs
@@ -32,12 +32,38 @@ app.MapGet("/assignments", async (AppDbContext db) =>
     }
 );
 
+/// <summary>
+/// Find by id a assignments
+/// </summary>
 app.MapGet("/assignments/{id}", async(int id, AppDbContext db) => await db.Assignments.FindAsync(id) is Assignment assignment ? Results.Ok(assignment) : Results.NotFound());
 
+
+/// <summary>
+///  find a assignments if concluded
+/// </summary>
 app.MapGet("/assignments/concluded", async (AppDbContext db) =>
     {
         var result = await db.Assignments.Where(a => a.IsConcluded == true).ToListAsync();
         return result;
+    }
+);
+
+/// <summary>
+/// update a assignments
+/// </summary>
+app.MapPut("/assignments/{id}", async (AppDbContext db, int id, Assignment assignments) =>
+    {
+        var result = await db.Assignments.FindAsync(id);
+
+        if(assignments is null)
+        {
+            return Results.NotFound();
+        }
+
+        assignments.Name = result?.Name; 
+        assignments.IsConcluded = result.IsConcluded;
+        await db.SaveChangesAsync();
+        return Results.NoContent();
     }
 );
 
@@ -49,6 +75,24 @@ app.MapPost("/assignments", async (Assignment assignment, AppDbContext db) =>
         db.Assignments.AddAsync(assignment);
         await db.SaveChangesAsync();
         return Results.Created($"/assignment/{assignment.Id}", assignment);
+    }
+);
+
+
+/// <summary>
+/// Remove by id a assignments
+/// </summary>
+app.MapDelete("/assignments/{id}", async (int id, AppDbContext db) =>
+    {
+        var result = await db.Assignments.FindAsync(id);
+
+        if (await db.Assignments.FindAsync(id) is Assignment assignment)
+        {
+            db.Assignments.Remove(assignment);
+            await db.SaveChangesAsync();
+            return Results.Ok(assignment);
+        }
+        return Results.NotFound();
     }
 );
 
